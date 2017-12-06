a137be99afa03c4c8221b0d8bc974217f86747f3
Katka <katkavalkovic@gmail.com>
2017-12-06 20:38:36 +0100

    zakladne zmeny

--- unit1.pas
+++ unit1.pas
@@ -13,8 +13,20 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
     Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
     Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
     Label1: TLabel;
   private
     { private declarations }
