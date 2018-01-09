a6748231969a8ad898f0366f2c55d1e40b4e4182
Katka <katkavalkovic@gmail.com>
2018-01-09 20:09:16 +0100

    vsetko po starom
    prepac za opatovne commitnutie vsetkych suborov, thats how i do it ked mi vypisuje chybu ktoru nepoznam ale nastastie mam zalohy....

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -221,33 +221,25 @@ Prihlasenie;
 end;
 
 procedure TForm1.VyhladajBtnClick(Sender: TObject);
-var
-  mameTovar: boolean;
 begin
 memo2.clear;
-mameTovar:= False;
-Label2.Visible:= False;
+//co keby teraz vybehlo memo s nazvom tovaru a uz nan nemusim klikat, ved som ho uz vybral <=== presne takto som to mal v plane
+//vypise to aj do Labelu kde je nazov tovaru
+//vyhlada tovar v zozname tovaru a zisti jeho index, zaznamena ho
+//globalna premenna index bude pouzita do VLOZ
    ListBox1.Items.Clear;
    kodTovaru:=Edit3.Text;
    for i:=1 to m do
-      if kodTovaru = tovar[ i ].kod then
+      if kodTovaru = tovar[i].kod then
          begin
-           mameTovar:= true;
-           if tovar[ i ].pocet > 0 then
-             begin
-               Label2.Caption:= tovar[ i ].nazov;
-               Memo2.visible:= true;
-               Memo2.append(tovar[ i ].nazov);
-               Label2.Visible:= True;
-               itemIndex:= i - 1;
-             end
-           else
-             begin
-               ShowMessage('Prepáčte, ale tento tovar momentálne nemáme na sklade.');
-             end;
+           Label2.Caption:= tovar[i].nazov;
+           Memo2.visible:=true;
+           Memo2.append(tovar[i].nazov);
+           Label2.Visible:= True;
+           itemIndex:= i - 1;
+           //-1 preto, lebo v procedure VlozBtnClick je z nejakeho dovodu: "itemIndex = itemIndex + 1;"
+           //a nechcel som do toho rypat, ale mali by sme to odstranit asi
          end;
-   if mameTovar = false then
-         ShowMessage('Tovar s takýmto kódom neexistuje');
 
 end;
 
@@ -370,7 +362,7 @@ begin
 Memo2.visible:=false;
 ListBox1.Items.Clear;
 for i:=1 to m do
-    ListBox1.Items.Add(tovar[i].nazov+'       '+inttostr(tovar[i].pocet));
+    ListBox1.Items.Add(tovar[i].nazov+'       '+tovar[i].pocet);
 end;
 
 procedure TForm1.StornoBtnClick(Sender: TObject);
